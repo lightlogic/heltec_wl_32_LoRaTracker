f7c8a0f2a153f1a383bc95cda036af3405f553b9
Frederic Noyer <fred@lightlogic.ch>
2024-02-04 07:00:56 +0100

    variable typo + better template documentation

--- include__secrets.h
+++ include__secrets.h
@@ -1,6 +1,12 @@
-// modify the values according to you configuration and create a copy of this file
-// under ./include/secrets.h
-// use it by adding '#include "secrets.h"' in your files (! and not include/secrets.h!)
+/*
+    Modify the values according to your LoRa configuration
+    - Make a copy of this file as ./include/secrets.h
+    - Do not forget to add ./include/secrets.h to .gitignore
+    - To use the value in for ex. main.cpp, add #include "secrets.h"
+    
+    (i.e. just "secrets.h" and not "include/secrets.h". 
+    The .include folder is in the include patha allready.)
+*/ 
 
 #define DEV_EUI 0xDC, 0xDC, 0xDC, 0xDC, 0xDC, 0xDC, 0xDC, 0xDC
 #define APP_EUI 0xDC, 0xDC, 0xDC, 0xDC, 0xDC, 0xDC, 0xDC, 0xDC
